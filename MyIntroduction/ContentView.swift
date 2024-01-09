d013a8b84080eff971b24261b6881f152e063d7e
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2024-01-09 10:55:53 +0700

    Initial Commit

---
+++ MyIntroduction/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyIntroduction
+//
+//  Created by irfan wahendra on 09/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyIntroduction/MyIntroductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyIntroductionApp.swift
+//  MyIntroduction
+//
+//  Created by irfan wahendra on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MyIntroductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
